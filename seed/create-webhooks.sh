f055ba9294daaaf84a561dab0e06fed79e8bd357
Colm Lynch <colm.lynch@justice.gov.uk>
2025-08-28 10:49:15 +0100

    feat:Refactor authentication

--- seed/create-webhooks.sh
+++ seed/create-webhooks.sh
@@ -3,9 +3,9 @@
 PACT_BROKER_USERNAME=$(kubectl get secret laa-data-pact-broker-secrets -n $NAMESPACE -o jsonpath='{.data.PACT_BROKER_BASIC_AUTH_USERNAME}'  | base64 --decode)
 PACT_BROKER_PASSWORD=$(kubectl get secret laa-data-pact-broker-secrets -n $NAMESPACE -o jsonpath='{.data.PACT_BROKER_BASIC_AUTH_PASSWORD}'  | base64 --decode)
 
-if [ "$GITHUB_ACCESS_TOKEN" = "" ] || [ "$PACT_BROKER_USERNAME" = "" ] || [ "$PACT_BROKER_PASSWORD" = "" ]; then
+if [ "GH_PAT_ACCESS_TOKEN" = "" ] || [ "$PACT_BROKER_USERNAME" = "" ] || [ "$PACT_BROKER_PASSWORD" = "" ]; then
   echo "One or more environment variables are missing. Usage:"
-  echo "GITHUB_ACCESS_TOKEN=token PACT_BROKER_USERNAME=user PACT_BROKER_PASSWORD=password $0"
+  echo "GH_PAT_ACCESS_TOKEN=token PACT_BROKER_USERNAME=user PACT_BROKER_PASSWORD=password $0"
   exit 1
 fi
 
@@ -13,12 +13,7 @@ function upsert_webhook() {
   local file="$1"
   local webhookID="$2"
   echo "✨ applying $file..."
-  echo "First: ${GITHUB_ACCESS_TOKEN:0:1}."
-  echo "Last: ${GITHUB_ACCESS_TOKEN: -1}."
-
-  echo "First PAT: ${GH_PAT_ACCESS_TOKEN:0:1}."
-  echo "Last PAT: ${GH_PAT_ACCESS_TOKEN: -1}."
-  sed "s/\${GITHUB_ACCESS_TOKEN}/$GITHUB_ACCESS_TOKEN/; s/\${GH_PAT_ACCESS_TOKEN}/$GH_PAT_ACCESS_TOKEN/" "$file" |
+  sed "s/\${GH_PAT_ACCESS_TOKEN}/$GH_PAT_ACCESS_TOKEN/" "$file" |
     curl -X PUT \
       "https://laa-data-pact-broker.apps.live.cloud-platform.service.justice.gov.uk/webhooks/$webhookID" \
       --user "$PACT_BROKER_USERNAME:$PACT_BROKER_PASSWORD" \
@@ -41,4 +36,4 @@ function delete_webhook() {
 # these ".../webhooks/ID" IDs are randomly chosen -- they will be either "created or updated" so pick anything for new webhooks
 # for pedantics: Pact generates these via `SecureRandom.urlsafe_base64`: https://ruby-doc.org/stdlib-3.0.1/libdoc/securerandom/rdoc/Random/Formatter.html#method-i-urlsafe_base64
 upsert_webhook "webhook-laa-data-provider-data-service.json" "WG8tj0CBNP_3zP9BwJmC7g"
-upsert_webhook "webhook-laa-data-provider-data-service_temp.json" "kd9BlR3Ec5-JIBfuMCgM6g"
+delete_webhook "kd9BlR3Ec5-JIBfuMCgM6g"
